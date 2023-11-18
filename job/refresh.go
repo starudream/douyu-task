b77546741018335d97bd0b8c280399af021ffac1
sheng.wang <justwangsheng@gmail.com>
2023-11-18 16:56:55 +0800

    feat: add refresh retry

--- job/refresh.go
+++ job/refresh.go
@@ -2,12 +2,20 @@ package job
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/starudream/go-lib/core/v2/slog"
+	"github.com/starudream/go-lib/core/v2/utils/timeutil"
 
 	"github.com/starudream/douyu-task/api"
 	"github.com/starudream/douyu-task/config"
 	"github.com/starudream/douyu-task/ws"
+)
+
+const (
+	RefreshLoop        = 5
+	RefreshMinDuration = 3 * time.Second
+	RefreshMaxDuration = 8 * time.Second
 )
 
 func Refresh() error {
@@ -22,6 +30,10 @@ func Refresh() error {
 	}
 	slog.Info("gifts before:\n%s", gifts1.TableString())
 
+	loopCount := 0
+
+login:
+
 	err = ws.Login(ws.LoginParams{
 		Room:     config.C().Douyu.Room,
 		Stk:      client.Stk,
@@ -29,7 +41,15 @@ func Refresh() error {
 		Username: client.Username,
 	})
 	if err != nil {
-		return fmt.Errorf("login error: %w", err)
+		if loopCount >= RefreshLoop {
+			return fmt.Errorf("login error: %w", err)
+		}
+		slog.Error("login error when attempt %d: %v", loopCount, err)
+		loopCount++
+		duration := timeutil.JitterDuration(RefreshMinDuration, RefreshMaxDuration, loopCount)
+		slog.Info("login will retry after %s", duration.String())
+		time.Sleep(duration)
+		goto login
 	}
 
 	gifts2, err := client.ListGifts()
